aace1ed86a7a1f4952a285bdc9599ca07921ab46
danhamill <hamill.daniel@gmail.com>
2016-08-23 17:49:44 -0700

    window analysis importing tools

---
+++ 40_import_chunks.sh
@@ -0,0 +1,86 @@
+#list of side scan and texture chunks
+ssfiles=$(find C:/workspace/Reach_4a/window_analysis/2014_04/40/ | egrep "R[0-9]{5}x_y_ss_raw[0-9]{1,2}.asc")
+texfiles=$(find C:/workspace/Reach_4a/window_analysis/2014_04/40/ | egrep "*R[0-9]{5}x_y_class[0-9]{1,2}.asc")
+
+#File paths for testing individual files
+#ssfile=$(find C:/Users/dan/Desktop/New_Folder/Sept_2014/ | egrep "R01769x_y_ss_raw0.asc")
+#texfile=$(find C:/Users/dan/Desktop/New_Folder/Sept_2014/ | egrep "*R01769x_y_class0.asc")
+
+#Build Tables
+survey='wa_40'
+
+tablename='tmp11'
+tablename2='tmp211'
+gist='_the_geom_gist'
+
+psql -h localhost -d reach_4a -U root -p 9000 -c "CREATE TABLE "$survey"( gid SERIAL NOT NULL,
+ easting double precision, northing double precision, texture double precision, sidescan_intensity double precision, the_geom GEOMETRY, scan_line text, CONSTRAINT enforce_dims_the_geom CHECK (st_ndims(the_geom) = 2), CONSTRAINT enforce_geotype_geom CHECK (geometrytype(the_geom) = 'POINT'::text OR the_geom IS NULL), CONSTRAINT enforce_srid_the_geom CHECK (st_srid(the_geom) = 26949));"
+
+psql -h localhost -d reach_4a -U root -p 9000 -c "CREATE TABLE "$tablename"(gid SERIAL NOT NULL, easting double precision, northing double precision, texture double precision, sidescan_intensity double precision, scan_line text);"
+
+psql -h localhost -d reach_4a -U root -p 9000 -c "CREATE TABLE "$tablename2"(
+  gid SERIAL NOT NULL,
+  easting double precision,
+  northing double precision,
+  sidescan_intensity double precision,
+  scan_line text
+);"
+
+
+#Array of side scan and texture chunks
+array1=($ssfiles)
+array2=($texfiles)
+
+for i in "${!array1[@]}"; do
+
+#set variable tex chunks file path
+texfile=${array2[$i]}
+echo $texfile
+
+#Parse scan line from file name
+filename="${texfile##*/}"
+name="${filename%x_y_class*.asc}"
+
+
+#Import texture chunk
+psql -h localhost -d reach_4a -U root -p 9000 -c "\COPY "$tablename" (easting,northing,texture) FROM "$texfile" DELIMITER ' ' CSV;"
+
+#Update texture table field with respective scan line of the chunk
+psql -h localhost -d reach_4a -U root -p 9000 -c "UPDATE "$tablename" SET scan_line= '"$name"' WHERE scan_line IS NULL;"
+psql -h localhost -d reach_4a -U root -p 9000 -c "VACUUM "$tablename";"
+
+#set variable ss chunks file path
+ssfile=${array1[$i]} 
+echo $ssfile
+
+#Import side scan chunk
+psql -h localhost -d reach_4a -U root -p 9000 -c "\COPY "$tablename2" (easting,northing,sidescan_intensity) FROM "$ssfile" DELIMITER ' ' CSV;"
+psql -h localhost -d reach_4a -U root -p 9000 -c "VACUUM "$tablename2";"
+
+#Update texture chunk with SS intensity
+psql -h localhost -d reach_4a -U root -p 9000 -c "UPDATE "$tablename" SET sidescan_intensity="$tablename2".sidescan_intensity FROM "$tablename2" WHERE "$tablename2".easting="$tablename".easting AND "$tablename2".northing="$tablename".northing;"
+
+echo "Successfully merged side scan and texture chunks"
+
+#Append to survey year main table
+psql -h localhost -d reach_4a -U root -p 9000 -c "INSERT INTO "$survey" (easting,northing,texture,sidescan_intensity,scan_line) (SELECT easting,northing,texture,sidescan_intensity,scan_line FROM "$tablename");"
+
+#Delete all data from temporary tables
+psql -h localhost -d reach_4a -U root -p 9000 -c "DELETE FROM "$tablename";"
+psql -h localhost -d reach_4a -U root -p 9000 -c "DELETE FROM "$tablename2";"
+
+#Maintenance temporary tables for improved efficiency
+psql -h localhost -d reach_4a -U root -p 9000 -c "VACUUM "$tablename";"
+psql -h localhost -d reach_4a -U root -p 9000 -c "VACUUM "$tablename2";"
+psql -h localhost -d reach_4a -U root -p 9000 -c "VACUUM "$survey";"
+done
+
+echo "Done importing chunks!!!"
+#Create mosaic table
+# psql -h localhost -d reach_4a -U root -p 9000 -c "CREATE TABLE "$mosaic" AS(SELECT tt.* FROM "$survey" tt INNER JOIN (SELECT easting, northing,texture, scan_line, MAX(sidescan_intensity) AS MaxSSIntensity FROM "$survey" GROUP BY (easting,northing,texture,scan_line)) groupedtt ON tt.easting = groupedtt.easting AND tt.northing = groupedtt.northing AND tt.texture = groupedtt.texture AND tt.scan_line = groupedtt.scan_line AND tt.sidescan_intensity = groupedtt.MaxSSIntensity);"
+
+
+
+# #Drop temporary tables
+psql -h localhost -d reach_4a -U root -p 9000 -c "DROP TABLE "$tablename" ;"
+psql -h localhost -d reach_4a -U root -p 9000 -c "DROP TABLE "$tablename2" ;"
